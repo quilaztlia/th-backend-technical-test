7e210f04e7bfc8ffd017116e3025d8e78b890a9f
Adriana NAVA AGUILAR <tennamiqui@hotmail.com>
2023-01-31 22:53:45 +0100

    contact api: getOne, getAll, post, put & delete

--- apps/contacts/serializers.py
+++ apps/contacts/serializers.py
@@ -6,7 +6,7 @@ from rest_framework import serializers
 class ContactSerializer(serializers.ModelSerializer):
     class Meta:
         model = Contact
-        fields = ['email', 'first_name', 'last_name', 'phone_number'] 
+        fields = ['id', 'email', 'first_name', 'last_name', 'phone_number'] 
 
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
--- apps/contacts/urls.py
+++ apps/contacts/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
 from rest_framework import routers
-from .views import ContactListApiView
+from .views import ContactListApiView, ContactDetailApiView
 
 #router = routers.DefaultRouter()
 #router.register(r'users', views.UserViewSet)
 #routers.register(r'groups', views.)
 
 urlpatterns = [    
-    path('api', ContactListApiView.as_view())
-    #path('contacts/<uuid:pk>', include(contacts_urls)),
+    path('api', ContactListApiView.as_view()),
+    path('api/<int:contact_id>', ContactDetailApiView.as_view())
 ]
