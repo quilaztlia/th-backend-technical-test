da83187d678bf404af51be155368093db46c3515
Adriana NAVA AGUILAR <tennamiqui@hotmail.com>
2023-01-31 16:58:04 +0100

    adding: meeting serializer + view + url

--- apps/meetings/urls.py
+++ apps/meetings/urls.py
@@ -1,6 +1,14 @@
+#from django.conf.urls import url
 from django.urls import path
+#from django.urls import path, include
+from .views import (
+    MeetingListApiView,
+)
+
+
 from . import views
 
 urlpatterns = [
-   path('', views.meetings_list, name='meetings_list.html'),
+   #path('', views.meetings_list, name='meetings_list.html'),
+   path('api', MeetingListApiView.as_view()),
 ]
--- apps/meetings/views.py
+++ apps/meetings/views.py
@@ -1,7 +1,42 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from apps.meetings.models import Meeting
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status, permissions
 
-def meetings_list(request):
-    meetings = Meeting.objects.all()
-    return render(request, 'meetings/meetings_list.html', {'meetings': meetings})    
+#from django.shortcuts import render
+#from django.http import HttpResponse
+from apps.meetings.models import Meeting
+from apps.meetings.serializers import MeetingSerializer
+
+class MeetingListApiView(APIView):
+    permission_classes = [permissions.IsAuthenticated]
+
+    def meetings_list(request):
+        meetings = Meeting.objects.all()
+        return render(request, 'meetings/meetings_list.html', {'meetings': meetings})    
+
+    def get(self, request, *args, **kwargs):
+        #meetings = Meeting.objects.filter(user = request.user.id)        
+        meetings = Meeting.objects.all()      
+        serializers = MeetingSerializer(meetings, many=True)
+        return Response(serializers.data, status=status.HTTP_200_OK)
+
+    # 2. Create
+    def post(self, request, *args, **kwargs):        
+        data = {
+            'task': request.data.get('task'), 
+            'completed': request.data.get('completed'), 
+            #'user': request.user.id,
+            'author': request.data.get('author'), 
+            'title': request.data.get('title'), 
+            'description': request.data.get('description'), 
+            'datetime': request.data.get('datetime'), 
+            'duration': request.data.get('duration'), 
+            'status': request.data.get('status'), 
+            'contacts': request.data.get('contacts')
+        }
+        serializers = MeetingSerializer(data=data)
+        if serializers.is_valid():
+            serializers.save()
+            return Response(serializers.data, status=status.HTTP_201_CREATED)
+
+        return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST) 
