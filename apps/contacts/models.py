34090cd4fbc893aede1b4c169abda47492d1bb4b
Adriana NAVA AGUILAR <tennamiqui@hotmail.com>
2023-01-31 16:13:12 +0100

    add contact & meeting models

--- apps/contacts/models.py
+++ apps/contacts/models.py
@@ -1,15 +1,12 @@
 from django.db import models
-#from apps.meetings.models import Meeting
-#from phonenumber_field.modelfields import PhoneNumberField
-    
-class Contact(models.Model):
-    first_name = models.CharField(max_length=200, blank=False, null=False)
-    last_name = models.CharField(max_length=200, blank=False, null=False)
+#from phonenumber_field.modelfields import PhoneNumberField #https://django.fun/en/qa/107946/
+
+class Contact(models.Model):   
     email = models.EmailField(blank=False, null=False, unique=True)
-    #phone_number = PhoneNumberField(blank=True)
+    first_name = models.CharField(max_length=200, blank=False, null=False)
+    last_name = models.CharField(max_length=200, blank=False, null=False)         
     #phone_number = PhoneNumberField()
-    phone_number = models.CharField(max_length=15, blank=True)     
-    #meetings = models.ForeignKey(Meeting, on_delete=models.RESTRICT)
+    phone_number = models.CharField(max_length=50, blank=True)     
     
     def __str__(self):
-        return f'{self.first_name} - {self.last_name}'
+        return f'{self.email} : {self.first_name} - {self.last_name}'
--- apps/meetings/models.py
+++ apps/meetings/models.py
@@ -2,10 +2,12 @@ from django.db import models
 from django.conf import settings
 from django.utils import timezone
 from apps.contacts.models import Contact
+#from django.contrib.auth.models import User
 
 #30min : magic String! => Constants
 class Meeting(models.Model):
-    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE) #TODO: delete?    
+    #TODO: delete?
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)     
 
     TODO = 'Todo'
     CANC = 'Canceled'
@@ -18,12 +20,12 @@ class Meeting(models.Model):
         (COMP, 'Completed'),
     ]
 
-    title = models.CharField(max_length=200, blank=False, null=False) 
+    title = models.CharField(max_length=200, blank=False, null=False, unique=True) 
     description = models.TextField(blank=True, null=False, default='') 
     datetime = models.DateTimeField(blank=False, null=False, default=timezone.now())
     duration = models.DurationField(blank=False, null=False, default=30) 
     status = models.CharField(max_length=9, choices= MEETING_STATUS_CHOICES, default=TODO)
-    contacts = models.ForeignKey(Contact, on_delete=models.RESTRICT) #(you can have a meeting with several contacts)    
+    contacts = models.ManyToManyField(Contact, None)
 
     def __str__(self):
         return f'{self.title} at {self.datetime}'
