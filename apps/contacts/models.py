8051206e98af6781970ecc2ab466cb88e6798f73
Adriana NAVA AGUILAR <tennamiqui@hotmail.com>
2023-02-01 17:10:30 +0100

    contact: cleaning view & model

--- apps/contacts/models.py
+++ apps/contacts/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-#from phonenumber_field.modelfields import PhoneNumberField 
+#TODO: from phonenumber_field.modelfields import PhoneNumberField 
 # https://django.fun/en/qa/107946/
 
 class Contact(models.Model):   
@@ -7,7 +7,7 @@ class Contact(models.Model):
     email = models.EmailField(blank=False, null=False, unique=True)
     first_name = models.CharField(max_length=200, blank=False, null=False)
     last_name = models.CharField(max_length=200, blank=False, null=False)         
-    #phone_number = PhoneNumberField()
+    #phone_number = PhoneNumberField()#TODO:?
     phone_number = models.CharField(max_length=50, blank=True)     
     
     def __str__(self):
--- apps/contacts/views.py
+++ apps/contacts/views.py
@@ -19,14 +19,13 @@ class ContactListApiView(APIView):
         return render(request, 'contacts/contacts_list.html', {'contacts': contacts})    
 
     def get(self, request, *args, **kwargs):
-        #contacts = Contact.objects.filter(user=request.user.id)
-        contacts = Contact.objects.all()
-        serializers = ContactSerializer(contacts, many=True)
-        return Response(serializers.data, status=status.HTTP_200_OK)
+            #contacts = Contact.objects.filter(user=request.user.id)
+            contacts = Contact.objects.all()
+            serializers = ContactSerializer(contacts, many=True)
+            return Response(serializers.data, status=status.HTTP_200_OK)
 
     def post(self, request, *args, **kwargs):
-        data = {   
-            #'id': request.data.get('id')         
+        data = {        
             'email' : request.data.get('email'),
             'first_name' : request.data.get('first_name'),
             'last_name' : request.data.get('last_name'),
@@ -37,3 +36,48 @@ class ContactListApiView(APIView):
             serializers.save()
             return Response(serializers.data, status=status.HTTP_201_CREATED)
         return Response(serializers.data, status=status.HTTP_400_BAD_REQUEST)
+
+class ContactDetailApiView(APIView):
+    permissions_classes = [permissions.IsAuthenticated]
+
+    def get_object(self, contact_id, user_id):
+        try:
+            return Contact.objects.get(id=contact_id)           
+        except Contact.DoesNotExist:
+            return None 
+        
+    def get(self, request, contact_id, *args, **kwargs):
+        contact = self.get_object(contact_id, request.user.id) 
+        if not contact:
+            return Response({"res": "This contact does not exists"},   status=status.HTTP_400_BAD_REQUEST)       
+        
+        serializers = ContactSerializer(contact, many=False)
+        return Response(serializers.data, status=status.HTTP_200_OK)    
+
+    def put(self, request, contact_id, *args, **kwargs):
+        contact = self.get_object(contact_id, request.user.id)
+        if not contact:
+            return Response({"res": "This contact does not exists"},   status=status.HTTP_400_BAD_REQUEST)       
+        
+        data = {
+            'email' : request.data.get('email'),
+            'first_name' : request.data.get('first_name'),
+            'last_name' : request.data.get('last_name'),
+            'phone_number' : request.data.get('phone_number')
+        }   
+        serializers = ContactSerializer(instance=contact, data=data, partial=True)
+        
+        if serializers.is_valid():
+            serializers.save()
+            return Response(serializers.data, status=status.HTTP_200_OK)
+
+        return Response(serializers.data, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, contact_id, *args, **kwargs):
+        contact = self.get_object(contact_id, request.user.id)
+        if not contact:
+            return Response({"res": "This contact does not exists"},   status=status.HTTP_400_BAD_REQUEST)       
+        
+        contact.delete()
+        return Response(status=status.HTTP_200_OK)
+        
